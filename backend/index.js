a9037b58c9930d984ed5003e670e98875efe33e7
rickymato22 <rickymahto103@gmail.com>
2025-10-06 13:46:45 +0530

    production ready app

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,15 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
+
 
 dotenv.config({});
-
+connectDB();
+const PORT = process.env.PORT || 8080;
 const app = express();
+
+const _dirname = path.resolve();
 
 // middleware
 app.use(express.json());
@@ -23,18 +28,17 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
-
-
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
+app.use(express.static(path.join(_dirname, "./frontend/dist")));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(_dirname, "frontend","dist","index.html"));
+});
 
 app.listen(PORT,()=>{
-    connectDB();
     console.log(`Server running at port ${PORT}`);
 })
